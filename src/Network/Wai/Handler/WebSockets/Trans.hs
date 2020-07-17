480858e82c7d7212db6246158581157f952d016b
Felix Springer <felixspringer149@gmail.com>
2020-07-17 16:47:50 +0200

    add documentation and change declaration order

--- src/Network/Wai/Handler/WebSockets/Trans.hs
+++ src/Network/Wai/Handler/WebSockets/Trans.hs
@@ -1,6 +1,21 @@
 {-# LANGUAGE FlexibleContexts #-}
 
-module Network.Wai.Handler.WebSockets.Trans where
+module Network.Wai.Handler.WebSockets.Trans (
+
+-- * ServerApp
+  ServerAppT
+, liftServerApp
+, runServerAppT
+
+-- * ClientApp
+, ClientAppT
+, liftClientApp
+, runClientAppT
+
+-- * WebSocket
+, websocketsOrT
+
+) where
 
 -- TODO: credit wai-transformers: https://github.com/athanclark/wai-transformers
 
@@ -11,32 +26,40 @@ import Network.WebSockets
 
 import Network.Wai.Trans
 
+-- | A type synonym for a websockets 'ServerApp' which has been lifted from the IO monad.
 type ServerAppT m = PendingConnection -> m ()
 
+-- | Lift a websockets 'ServerApp' to a 'ServerAppT'.
 liftServerApp :: MonadBase IO m
               => ServerApp
               -> ServerAppT m
 liftServerApp serverApp = liftBase . serverApp
 
+-- | Run a 'ServerAppT' in the inner monad.
 runServerAppT :: MonadBaseControlIdentity IO m
               => ServerAppT m
               -> m ServerApp
 runServerAppT serverAppT = liftBaseWithIdentity $ \ runInBase ->
   return $ runInBase . serverAppT
 
+-- | A type synonym for a websockets 'ClientApp' which has been lifted from the IO monad.
 type ClientAppT m a = Connection -> m a
 
+-- | Lift a websockets 'ClientApp' to a 'ClientAppT'.
 liftClientApp :: MonadBase IO m
               => ClientApp a
               -> ClientAppT m a
 liftClientApp clientApp = liftBase . clientApp
 
+-- | Run a 'ClientAppT' in the inner monad.
 runClientAppT :: MonadBaseControlIdentity IO m
               => ClientAppT m a
               -> m (ClientApp a)
 runClientAppT clientAppT = liftBaseWithIdentity $ \ runInBase ->
   return $ runInBase . clientAppT
 
+-- | Upgrade a 'ServerAppT' to a 'MiddlewareT'.
+-- This function is based on 'websocketsOr'.
 websocketsOrT :: MonadBaseControlIdentity IO m
               => ConnectionOptions
               -> ServerAppT m
--- src/Network/Wai/Trans.hs
+++ src/Network/Wai/Trans.hs
@@ -2,21 +2,45 @@
 
 -- TODO: credit wai-transformers: https://github.com/athanclark/wai-transformers
 
-module Network.Wai.Trans where
+module Network.Wai.Trans (
+
+  -- * Application
+  ApplicationT
+, liftApplication
+, runApplicationT
+
+  -- * Middleware
+, MiddlewareT
+, liftMiddleware
+, runMiddlewareT
+
+) where
 
 import Control.Monad.Base
 import Control.Monad.Trans.Control.Identity
 import Network.Wai
 
+-- | A type synonym for a wai 'Application' which has been lifted from the 'IO' monad.
 type ApplicationT m = Request -> (Response -> m ResponseReceived) -> m ResponseReceived
-type MiddlewareT m = ApplicationT m -> ApplicationT m
 
+-- | Lift a wai 'Application' to an 'ApplicationT'.
 liftApplication :: MonadBaseControlIdentity IO m
                 => Application
                 -> ApplicationT m
 liftApplication app request respond = liftBaseWithIdentity $ \ runInBase ->
   app request $ runInBase . respond
 
+-- | Run an 'ApplicationT' in the inner monad.
+runApplicationT :: MonadBaseControlIdentity IO m
+                => ApplicationT m
+                -> m Application
+runApplicationT appT = liftBaseWithIdentity $ \ runInBase ->
+  return $ \ request respond -> runInBase $ appT request $ liftBase . respond
+
+-- | A type synonym for a wai 'Middleware' which has been lifted from the 'IO' monad.
+type MiddlewareT m = ApplicationT m -> ApplicationT m
+
+-- | Lift a wai 'Middleware to an 'MiddlewareT'.
 liftMiddleware :: MonadBaseControlIdentity IO m
                => Middleware
                -> MiddlewareT m
@@ -24,12 +48,7 @@ liftMiddleware mid appT request respond = do
   app <- runApplicationT appT
   liftBaseWithIdentity $ \ runInBase -> mid app request $ runInBase . respond
 
-runApplicationT :: MonadBaseControlIdentity IO m
-                => ApplicationT m
-                -> m Application
-runApplicationT appT = liftBaseWithIdentity $ \ runInBase ->
-  return $ \ request respond -> runInBase $ appT request $ liftBase . respond
-
+-- | Run an 'MiddlewareT' in the inner monad.
 runMiddlewareT :: MonadBaseControlIdentity IO m
                => MiddlewareT m
                -> m Middleware
