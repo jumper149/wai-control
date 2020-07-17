851e381819b6e4c8c8bd9357f339365ff3085874
Felix Springer <felixspringer149@gmail.com>
2020-07-17 16:11:59 +0200

    add nix files, use monad-control-identity from git

---
+++ default.nix
@@ -0,0 +1,14 @@
+with import <nixpkgs> {};
+let
+  src-monad-control-identity = fetchGit {
+    url = "https://github.com/jumper149/monad-control-identity.git";
+    ref = "master";
+    rev = "15949d07dedbf95ad6536ac84be4d2f18dfa9523";
+  };
+  ghc = pkgs.haskell.packages.ghc883.override {
+    overrides = self: super: {
+      monad-control-identity = self.callCabal2nix "monad-control-identity" "${src-monad-control-identity}" {};
+    };
+  };
+in
+  ghc.callCabal2nix "wai-control" ./. {}
